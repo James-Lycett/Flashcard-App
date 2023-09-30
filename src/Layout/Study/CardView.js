9a45a24a0603769305790f6ac91effdb8e9d5b47
James-Lycett <jrlycett@mail.com>
2023-09-30 16:16:11 -0700

    fully functional study page. Added next button, fixed conditional rendering issue

--- src/Layout/Study/CardView.js
+++ src/Layout/Study/CardView.js
@@ -1,27 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { readCard } from "../../utils/api";
+import { useHistory } from "react-router-dom"
 
 function CardView( { deck } ) {
     const [card, setCard] = useState(deck.cards[0])
     const [cardNumber, setCardNumber] = useState(0)
     const [front, setFront] = useState(true)
-    console.log(deck)
+    const history = useHistory()
 
     useEffect(() => {
+        if (cardNumber !== deck.cards.length) {
         async function loadCard() {
             const response = await readCard(deck.cards[cardNumber].id)
             setCard(response)
         }
         loadCard()
-    }, [])
-
-    const cardTextHandler = () =>  {
-        if (front === true) {
-            return card.front
-        } else {
-            return card.back
         }
-    }
+    }, [cardNumber])
 
     const flipButtonHandler = () => {
         if (front === true) {
@@ -30,14 +25,30 @@ function CardView( { deck } ) {
             setFront(true)
         }
     }
+
+    const nextButtonHandler = () => {
+        setCardNumber(cardNumber + 1)
+        setFront(true)
+        if (cardNumber + 1 >= deck.cards.length) {
+            const confirm = window.confirm("Restart cards? Click 'cancel' to return to the home page.")
+            if (confirm) {
+                setCardNumber(0)
+            } else {
+                history.push("/")
+            }
+        }
+    }
+
     
+
     return (
         <>
         <div className="card" style={{width: "18rem"}}>
             <div className="card-body">
             <h5 className="card-title">{`Card ${cardNumber + 1} of ${deck.cards.length}`}</h5>
-            <p className="card-text">{cardTextHandler}</p>
+            <p className="card-text">{front ? card.front : card.back}</p>
             <a className="card-link"><button onClick={flipButtonHandler}>Flip</button></a>
+            {front === false ? <a className="card-link"><button onClick={nextButtonHandler}>Next</button></a> : null}
             </div>
         </div>
         </>
--- src/Layout/Study/Study.js
+++ src/Layout/Study/Study.js
@@ -7,24 +7,32 @@ import CardView from "./CardView";
 function Study() {
     const params = useParams()
     const deckId = params.deckId
-    const [deck, setDeck] = useState({})
+    const [deck, setDeck] = useState(null)
     
     useEffect(() => {
         async function loadDeck() {
-            const APIresponse = await readDeck(deckId)
-            setDeck(APIresponse)
-            console.log(deck)
+            try {
+                const APIresponse = await readDeck(deckId)
+                setDeck(APIresponse)
+                console.log(deck)
+            } catch (error) {
+                // handle any API errors here
+                console.log(error)
+            }
         }
         loadDeck()
-    }, [])
-    
-    return (
-        <>
-        <Breadcrumb deckName={deck.name}/>
-        <h2>{`Study: ${deck.name}`}</h2>
-        <CardView deck={deck}/>
-        </>
-    )
+    }, [deckId])
+    if (deck === null) {
+        return <p>Loading...</p>
+    } else {
+        return (
+            <>
+            <Breadcrumb deckName={deck.name}/>
+            <h2>{`Study: ${deck.name}`}</h2>
+            <CardView deck={deck}/>
+            </>
+        )
+    }
 }
 
 export default Study
