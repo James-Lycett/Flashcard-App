d0b8969f2e48cf41f37066cb0069580b46250cd5
James-Lycett <jrlycett@mail.com>
2024-01-06 07:31:05 -0800

    removed primary key ids from seeds

--- backend/src/db/seeds/02-decks.js
+++ backend/src/db/seeds/02-decks.js
@@ -8,14 +8,12 @@ exports.seed = async function(knex) {
   // Seeds a couple demo decks
   await knex('decks').insert([
     {
-      deck_id: 1,
       name: "Rendering in React",
       description: "React's component structure allows for quickly building a complex web application that relies on DOM manipulation. "
     },
     {
       name: "React Router",
       description: "React Router is a collection of navigational components that compose declaratively with your application.",
-      deck_id: 2
     }
   ]);
 };
