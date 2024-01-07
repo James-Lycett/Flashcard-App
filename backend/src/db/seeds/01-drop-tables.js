8355b838103ca68becdc9ac9b2611e0bd3cbcbcd
James-Lycett <jrlycett@mail.com>
2024-01-07 12:55:51 -0800

    set html doc description, added a favicon

--- backend/src/db/seeds/01-drop-tables.js
+++ backend/src/db/seeds/01-drop-tables.js
@@ -3,8 +3,13 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  return knex("decks")
-    .del()
-    .then(() => knex("cards").del())
+  // Kinda weird and risky raw SQL that seemed necessary to be able to run seeds repeatedly for deployment purposes
+    // Drops foreign key constraint
+    await knex.raw('ALTER TABLE cards DROP CONSTRAINT cards_deck_id_foreign');
+  
+    // Truncates tables
+    await knex.raw('TRUNCATE TABLE decks, cards RESTART IDENTITY');
+  
+    // Recreates foreign key constraint
+    await knex.raw('ALTER TABLE cards ADD CONSTRAINT cards_deck_id_foreign FOREIGN KEY (deck_id) REFERENCES decks(deck_id) ON UPDATE CASCADE ON DELETE CASCADE');
 };
