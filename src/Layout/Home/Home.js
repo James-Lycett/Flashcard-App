ed0506442581f5ca1ae6268e446f3c0dee35af0b
James-Lycett <jrlycett@mail.com>
2024-05-01 18:03:13 -0700

    added a 'server spins down' message

--- src/Layout/Home/Home.js
+++ src/Layout/Home/Home.js
@@ -10,11 +10,23 @@ function Home() {
 
     useEffect(() => {
         async function loadDecks() {
-            const APIresponse = await listDecks()
-            setDecks(APIresponse.data)
+            try {
+                const APIresponse = await listDecks()
+                setDecks(APIresponse.data)
+            }
+            catch (error) {
+                console.error(error)
+            }
         }
         loadDecks()
     }, [])
+
+    const loadingMessage = (
+        <>
+        <h4>Loading decks...</h4>
+        <p>The server spins down after a period of inactivity in order to save me money. If you don't see anything here give it a minute and reload the page. It's also possible that somebody deleted all my demo decks idk.</p>
+        </>
+    )
 
     return (
     <>
@@ -23,7 +35,7 @@ function Home() {
             <Link to="/decks/new"><button type="button" className="btn btn-secondary offset-lg-1 mb-5">+ Create Deck</button></Link>
         </div>
         <div>
-            {decks.length > 0 ? <DeckList decks={decks}/> : <h4>Loading decks...</h4>}
+            {decks.length > 0 ? <DeckList decks={decks}/> : loadingMessage}
         </div>
     </div>
     </>
